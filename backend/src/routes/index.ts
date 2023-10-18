95b7abf9c98a0b3b207af043ee296664c4295bc8
Tran Duoc <duoctran2527@gmail.com>
2023-10-18 15:04:28 +0700

    add admin router

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 
+import adminRouter from './admin.route'
 import userRouter from './user.route'
 
 const routers = Router()
 routers.use('/user', userRouter)
+routers.use('/admin', adminRouter)
 
 export default routers
