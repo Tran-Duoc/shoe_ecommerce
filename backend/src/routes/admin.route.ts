5c890b139ed2ac92547fc53b0885c5a2865abf5f
Tran Duoc <duoctran2527@gmail.com>
2023-10-19 22:05:41 +0700

    deploy to vervel

--- backend/src/routes/admin.route.ts
+++ backend/src/routes/admin.route.ts
@@ -1,5 +1,21 @@
 import { Router } from 'express'
 
+import { getPendingProduct, getUsersPublic } from '~/controllers/admin.controller'
+
+import { verifyAccessToken } from '../middlewares/auth.middleware'
+
 const router = Router()
+
+//1. get all user
+router.get('/users_public', verifyAccessToken, getUsersPublic)
+
+//2. checking product valid before public on website
+router.get('/checking_pending', verifyAccessToken, getPendingProduct)
+
+//3. handle pending
+router.post('/accept_pending_product/:id', verifyAccessToken)
+
+//4. block user
+router.post('/block_user/:id')
 
 export default router
